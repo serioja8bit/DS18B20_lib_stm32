40b4eaec2efe5c768bbe107fee5355c3e849c6cc
serioja8bit <93288400+serioja8bit@users.noreply.github.com>
2024-02-25 14:56:12 +0200

    Update ds18b20.h

--- ds18b20.h
+++ ds18b20.h
@@ -8,16 +8,16 @@
 #ifndef INC_DS18B20_H_
 #define INC_DS18B20_H_
 
-#define DS18B20_PORT GPIOA     	    //DS18B20Port Change with your ds18b20 port
-#define DS18B20_PIN  GPIO_PIN_1 	//DS18b20Pin  Change with your ds18b20 pin
-#define TIMER		 &htim4			//1Mhz timer  Change with your 1Mhz timer for 1 us delay function
+#define DS18B20_PORT GPIOA     	          //DS18B20Port Change with your ds18b20 port
+#define DS18B20_PIN  GPIO_PIN_1 	        //DS18b20Pin  Change with your ds18b20 pin
+#define TIMER		 &htim4			              //1Mhz timer  Change with your 1Mhz timer for 1 us delay function
 
 #include "tim.h"
 #include "stdio.h"
 
-void delay (uint32_t us);
-void Set_Pin_Output (GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin);
-void Set_Pin_Input (GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin);
+void delay (uint32_t us);                                      //this function is used to obtain x us (microseconds)
+void Set_Pin_Output (GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin);  //used to set GPIO pin as output
+void Set_Pin_Input (GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin);   //used to set GPIO pin as input
 uint8_t DS18B20_Start (void);
 void DS18B20_Write (uint8_t data);
 uint8_t DS18B20_Read (void);
